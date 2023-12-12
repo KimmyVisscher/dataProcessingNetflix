ec9c94ae4d38702fd31d04ecaadc14390891e2a1
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-12-12 09:43:21 +0100

    Switch from SQLAlchemy and Pydantic to SQLModel, movie and subtitle classes

--- main.py
+++ main.py
@@ -1,3 +1,72 @@
-from fastapi import FastAPI
+from typing import List, Optional
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
+
+from secrets import *
+
+
+class MovieBase(SQLModel):
+    title: str
+    movie_duration: int
+
+
+class Movie(MovieBase, table=True):
+    movie_id: Optional[int] = Field(default=None, primary_key=True)
+
+    subtitles: List["Movie"] = Relationship(back_populates="movie")
+
+
+class MovieRead(MovieBase):
+    movie_id: int
+
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class SubtitleBase(SQLModel):
+    language: str
+    subtitle_location: str
+
+    movie_id: Optional[int] = Field(default=None, foreign_key="Movie.movie_id")
+
+
+class Subtitle(SubtitleBase, table=True):
+    subtitle_id = Optional[int] = Field(default=None, primary_key=True)
+
+    movie: Optional[Movie] = Relationship(back_populates="subtitles")
+
+
+class SubtitleRead(SubtitleBase):
+    subtitle_id: int
+
+
+class SubtitleCreate(SubtitleBase):
+    pass
+
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(connect_string, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+app = FastAPI()
+
+
