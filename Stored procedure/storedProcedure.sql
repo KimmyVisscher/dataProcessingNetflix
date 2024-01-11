748dc14f9ee5541d14ca86bd2b57e8a23f7a11c4
KimmyVisscher <kimmy.visscher@student.nhlstenden.com>
2024-01-11 21:28:38 +0100

    started on stored procedures

--- Stored procedure/storedProcedure.sql
+++ Stored procedure/storedProcedure.sql
@@ -1,8 +1,33 @@
-CREATE PROCEDURE GetRevenue
-AS
-SELECT payment_method FROM account
-GO;
+DELIMITER //
+
+CREATE PROCEDURE calculateTotalRevenue()
+BEGIN
+    DECLARE totalRevenue FLOAT(10);
+
+    SELECT SUM(subscription_price) INTO totalRevenue
+    FROM subscription;
+
+    SELECT totalRevenue AS TotalRevenue;
+END //
+
+DELIMITER ;
 
 
-#Account table nodig, niet de subscription table nodig. 
-#In de account moeten kijken welke subscription diegene heeft, waarna je dit op telt. Daarna ga je dat x de prijzen doen.
+--^^calculate the total revenue^^--
+
+DELIMiTER //
+
+CREATE PROCEDURE calculateTotalAccounts()
+BEGIN
+    DECLARE totalAccounts INT;
+
+    SELECT COUNT(*) INTO totalAccounts
+    FROM account;
+
+    SELECT totalAccounts AS TotalAccounts;
+END //
+
+DELIMIER ;
+
+--^^calculate the total accounts^^--
+
