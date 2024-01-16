148232aa3426eada22630223106e0c98dee00ecf
jariolyslager <jari.olijslager@student.nhlstenden.com>
2024-01-16 15:10:18 +0100

    Deleted __init__.py and added the unit tests for the get-movie-by-id-endpoint

--- test_main.py
+++ test_main.py
@@ -1,3 +1,51 @@
 from fastapi.testclient import TestClient
 
-from .main import app
+from secrets import *
+
+from main import app
+
+
+client = TestClient(app)
+
+
+def test_read_movie_json_response():
+    response = client.get("/movies/1", headers={"X-API-KEY": "senior"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "title": "The Shawshank Redemption",
+        "movie_duration": 142,
+        "age_restriction": "SIXTEEN_YEARS",
+        "movie_id": 1
+    }
+
+
+def test_read_movie_xml_response():
+    response = client.get("/movies/1", headers={"X-API-KEY": "senior", "accept": "application/xml"})
+    assert response.status_code == 200
+    assert response.text == "<movies>\n  <movie>\n      <title>The Shawshank Redemption</title>\n      <movie_duration>142</movie_duration>\n      <age_restriction>AgeRestriction.SIXTEEN_YEARS</age_restriction>\n  </movie></movies>"
+
+
+def test_read_movie_unauthorized():
+    response = client.get("/movies/1")
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_read_movie_not_found():
+    response = client.get("/movies/999", headers={"X-API-KEY": "senior"})
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Movie not found"
+    }
+
+
+def test_read_movie_no_permission():
+    response = client.get("/movies/1", headers={"X-API-KEY": "senior"})
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+        # To test this I need to create a role in main.py with value < 1.
+        # Then make a api-key in the database with this role. Then we can run this test.
+    }
