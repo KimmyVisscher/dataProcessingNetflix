b462ad91112a184050bdf677f1134748e292394d
jariolyslager <jari.olijslager@student.nhlstenden.com>
2024-01-21 13:53:50 +0100

    Added unit tests for post, put & delete endpoints related to preferences

--- test_preferences.py
+++ test_preferences.py
@@ -287,18 +287,467 @@ def test_read_agepreference_by_id_no_permission():
 
 # POST genrepreference
 
+def test_create_genrepreference_success():
+    genrepreference_data = {
+        "genrepreference_id": 5,
+        "profile_id": 3,
+        "genre": "ACTION"
+    }
+
+    response = client.post("/profiles/3/genrepreferences", json=genrepreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == genrepreference_data
+
+
+def test_create_genrepreference_unauthorized():
+    genrepreference_data = {
+        "genrepreference_id": 5,
+        "profile_id": 3,
+        "genre": "ACTION"
+    }
+
+    response = client.post("/profiles/3/genrepreferences", json=genrepreference_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_create_genrepreference_not_found():
+    genrepreference_data = {
+        "genrepreference_id": 5,
+        "profile_id": 3,
+        "genre": "ACTION"
+    }
+
+    response = client.post("/profiles/999/genrepreferences", json=genrepreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Profile not found"
+    }
+
+
+def test_create_genrepreference_no_permission():
+    genrepreference_data = {
+        "genrepreference_id": 5,
+        "profile_id": 3,
+        "genre": "ACTION"
+    }
+
+    response = client.post("/profiles/3/genrepreferences", json=genrepreference_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
+
 # POST indicationpreference
+
+def test_create_indicationpreference_success():
+    indicationpreference_data = {
+        "indicationpreference_id": 5,
+        "profile_id": 3,
+        "indication": "VIOLENCE"
+    }
+
+    response = client.post("/profiles/3/indicationpreferences", json=indicationpreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == indicationpreference_data
+
+
+def test_create_indicationpreference_unauthorized():
+    indicationpreference_data = {
+        "indicationpreference_id": 5,
+        "profile_id": 3,
+        "indication": "VIOLENCE"
+    }
+
+    response = client.post("/profiles/3/indicationpreferences", json=indicationpreference_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_create_indicationpreference_not_found():
+    indicationpreference_data = {
+        "indicationpreference_id": 5,
+        "profile_id": 3,
+        "indication": "VIOLENCE"
+    }
+
+    response = client.post("/profiles/999/indicationpreferences", json=indicationpreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Profile not found"
+    }
+
+
+def test_create_indicationpreference_no_permission():
+    indicationpreference_data = {
+        "indicationpreference_id": 5,
+        "profile_id": 3,
+        "indication": "VIOLENCE"
+    }
+
+    response = client.post("/profiles/3/indicationpreferences", json=indicationpreference_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
 
 # POST agepreferences
 
+def test_create_agepreference_success():
+    agepreference_data = {
+        "agepreference_id": 5,
+        "profile_id": 3,
+        "agerestriction": "TWELVE_YEARS"
+    }
+
+    response = client.post("/profiles/3/agepreferences", json=agepreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == agepreference_data
+
+
+def test_create_agepreference_unauthorized():
+    agepreference_data = {
+        "agepreference_id": 5,
+        "profile_id": 3,
+        "agerestriction": "TWELVE_YEARS"
+    }
+
+    response = client.post("/profiles/3/agepreferences", json=agepreference_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_create_agepreference_not_found():
+    agepreference_data = {
+        "agepreference_id": 5,
+        "profile_id": 3,
+        "agerestriction": "TWELVE_YEARS"
+    }
+
+    response = client.post("/profiles/999/agepreferences", json=agepreference_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Profile not found"
+    }
+
+
+def test_create_agepreference_no_permission():
+    agepreference_data = {
+        "agepreference_id": 5,
+        "profile_id": 3,
+        "agerestriction": "TWELVE_YEARS"
+    }
+
+    response = client.post("/profiles/3/agepreferences", json=agepreference_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
+
 # PUT agepreference
+
+def test_update_agepreference_success():
+    updated_data = {
+        "agepreference_id": 2,
+        "profile_id": 1,
+        "agerestriction": "NINE_YEARS"
+    }
+
+    response = client.put("/agepreferences/2", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == updated_data
+
+
+def test_update_agepreference_unauthorized():
+    updated_data = {
+        "agepreference_id": 2,
+        "profile_id": 1,
+        "agerestriction": "NINE_YEARS"
+    }
+
+    response = client.put("/agepreferences/2", json=updated_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_update_agepreference_not_found():
+    updated_data = {
+        "agepreference_id": 999,
+        "profile_id": 1,
+        "agerestriction": "NINE_YEARS"
+    }
+
+    response = client.put("/agepreferences/999", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Agepreference not found"
+    }
+
+
+def test_update_agepreference_no_permission():
+    updated_data = {
+        "agepreference_id": 2,
+        "profile_id": 1,
+        "agerestriction": "NINE_YEARS"
+    }
+
+    response = client.put("/agepreferences/2", json=updated_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
 
 # DELETE agepreference
 
+def test_delete_agepreference_success():
+    response = client.delete("/agepreferences/4", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Agepreference deleted successfully"}
+
+
+def test_delete_agepreference_unauthorized():
+    response = client.delete("/agepreferences/4")
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_delete_agepreference_not_found():
+    response = client.delete("/agepreferences/999", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Agepreference not found"
+    }
+
+
+def test_delete_agepreference_no_permission():
+    response = client.delete("/agepreferences/4", headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
+
 # PUT indicationpreference
+
+def test_update_indicationpreference_success():
+    updated_data = {
+        "indicationpreference_id": 2,
+        "profile_id": 1,
+        "indication": "DISCRIMINATION"
+    }
+
+    response = client.put("/indicationpreferences/2", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == updated_data
+
+
+def test_update_indicationpreference_unauthorized():
+    updated_data = {
+        "indicationpreference_id": 2,
+        "profile_id": 1,
+        "indication": "DISCRIMINATION"
+    }
+
+    response = client.put("/indicationpreferences/2", json=updated_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_update_indicationpreference_not_found():
+    updated_data = {
+        "indicationpreference_id": 999,
+        "profile_id": 1,
+        "indication": "DISCRIMINATION"
+    }
+
+    response = client.put("/indicationpreferences/999", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Indicationpreference not found"
+    }
+
+
+def test_update_indicationpreference_no_permission():
+    updated_data = {
+        "indicationpreference_id": 2,
+        "profile_id": 1,
+        "indication": "DISCRIMINATION"
+    }
+
+    response = client.put("/indicationpreferences/2", json=updated_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
 
 # DELETE indicationpreference
 
+def test_delete_indicationpreference_success():
+    response = client.delete("/indicationpreferences/4", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Indicationpreference deleted successfully"}
+
+
+def test_delete_indicationpreference_unauthorized():
+    response = client.delete("/indicationpreferences/4")
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_delete_indicationpreference_not_found():
+    response = client.delete("/indicationpreferences/999", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Indicationpreference not found"
+    }
+
+
+def test_delete_indicationpreference_no_permission():
+    response = client.delete("/indicationpreferences/4", headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
+
 # PUT genrepreference
 
+def test_update_genrepreference_success():
+    updated_data = {
+        "genrepreference_id": 2,
+        "profile_id": 1,
+        "genre": "HORROR"
+    }
+
+    response = client.put("/genrepreferences/2", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == updated_data
+
+
+def test_update_genrepreference_unauthorized():
+    updated_data = {
+        "genrepreference_id": 2,
+        "profile_id": 1,
+        "genre": "HORROR"
+    }
+
+    response = client.put("/genrepreferences/2", json=updated_data)
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_update_genrepreference_not_found():
+    updated_data = {
+        "genrepreference_id": 999,
+        "profile_id": 1,
+        "genre": "HORROR"
+    }
+
+    response = client.put("/genrepreferences/999", json=updated_data, headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Genrepreference not found"
+    }
+
+
+def test_update_genrepreference_no_permission():
+    updated_data = {
+        "genrepreference_id": 2,
+        "profile_id": 1,
+        "genre": "HORROR"
+    }
+
+    response = client.put("/genrepreferences/2", json=updated_data, headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
+
+
 # DELETE genrepreference
+
+def test_delete_genrepreference_success():
+    response = client.delete("/genrepreferences/4", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Genrepreference deleted successfully"}
+
+
+def test_delete_genrepreference_unauthorized():
+    response = client.delete("/genrepreferences/4")
+
+    assert response.status_code == 401
+    assert response.json() == {
+        "detail": "Invalid API key"
+    }
+
+
+def test_delete_genrepreference_not_found():
+    response = client.delete("/genrepreferences/999", headers={"X-API-KEY": "senior"})
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Genrepreference not found"
+    }
+
+
+def test_delete_genrepreference_no_permission():
+    response = client.delete("/genrepreferences/4", headers={"X-API-KEY": "unauthorized"})
+
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "No permission"
+    }
